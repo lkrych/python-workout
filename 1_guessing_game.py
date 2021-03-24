778965eb6ac774bc13382a6ea54fa5cfb85e469a
Leland Krych <lkrych@cisco.com>
2021-03-24 08:19:00 -0700

    parameterize the guessing game to take num_guesses and highest_num as arguments

--- 1_guessing_game.py
+++ 1_guessing_game.py
@@ -1,20 +1,22 @@
 import random
 
-def guessing_game():
+def guessing_game(num_guesses, highest_num):
     #randint is inclusive
-    r = random.randint(1,100)
-    while True:
+    print(f"You have {num_guesses} to guess a number between 1 and {highest_num}")
+    r = random.randint(1,highest_num)
+    while num_guesses != 0:
         g = guess()
         if g == r:
-            break
+            print("you are correct!")
+            exit
         if g < r:
             print("too low")
         elif g > r: 
             print("too high")
-        continue
-    print("just right")
+        num_guesses -= 1
+    print("You did not guess the correct number")
 
 def guess():
     return int(input('Guess a number: '))
 
-guessing_game()
+guessing_game(3, 10)
