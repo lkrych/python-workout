7eacedbad5fc3470a844eacc668e234c49946640
Leland Krych <leland.krych@gmail.com>
2021-03-24 08:34:56 -0700

    begin adding support for hex input

--- 1_guessing_game.py
+++ 1_guessing_game.py
@@ -1,22 +1,31 @@
 import random
+from numpy import base_repr
 
 def guessing_game(num_guesses, highest_num):
     #randint is inclusive
-    print(f"You have {num_guesses} to guess a number between 1 and {highest_num}")
     r = random.randint(1,highest_num)
+    base = random.choice([2, 10, 16])
+    r = base_repr(r, base=base)
+    print(f"You have {num_guesses} to guess a number between 1 and {highest_num}")
+    print(f"You must enter your input with base: {base}")
     while num_guesses != 0:
-        g = guess()
+        g = guess(base)
+        print(f"comparing guess: {g}, with {r}")
         if g == r:
             print("you are correct!")
-            exit
+            exit(0)
         if g < r:
             print("too low")
         elif g > r: 
             print("too high")
         num_guesses -= 1
-    print("You did not guess the correct number")
+    print(f"You did not guess the correct number, it was {r}")
 
-def guess():
-    return int(input('Guess a number: '))
+def guess(base):
+    user_input = input(f'Guess a number (please use base {base}): ')
+    if base == 16:
+        # in case folks use lower case hex input
+        user_input = user_input.upper()
+    return user_input
 
-guessing_game(3, 10)
+guessing_game(3, 16)
