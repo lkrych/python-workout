766dba141d23781b461ff7c260ced9ed28e16b6a
Leland Krych <lkrych@cisco.com>
2021-03-25 07:50:02 -0700

    add type annotations and extend function to use optional starting value

--- 2_sum.py
+++ 2_sum.py
@@ -1,7 +1,10 @@
-def mysum(*input: int):
-    sum = 0
+from typing import List
+
+def mysum(input: List[int], starting_val: int = 0) -> int:
+    sum = starting_val
     for num in input:
         sum += num
     return sum
 
-print(mysum(1,2,3))
+print(mysum([1,2,3]))
+print(mysum([1,2,3],4))
