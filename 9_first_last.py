13b39a5495ded473a040927beef2606b5760c47c
Leland Krych <leland.krych@gmail.com>
2021-06-13 08:22:11 -0700

    implement even_odd idxs

--- 9_first_last.py
+++ 9_first_last.py
@@ -1,7 +1,19 @@
 def firstlast(seq):
     return seq[:1] + seq[-1:]
 
-print(firstlast('abc'))
-print(firstlast([1,2,3]))
-print(firstlast((1,2,3)))
+def sum(seq):
+    sum = 0
+    for el in seq:
+        sum += el
+    return sum
 
+def sum_even_odd_idxs(seq):
+    evens = seq[::2]
+    odds = seq[1::2]
+    return [sum(odds), sum(evens)]
+
+#print(firstlast('abc'))
+#print(firstlast([1,2,3]))
+#print(firstlast((1,2,3)))
+
+print(sum_even_odd_idxs([1,2,3,4,5,6]))
