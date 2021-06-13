9219e6998c7aa653ff4b1db5c0368badc9c1b695
Leland Krych <leland.krych@gmail.com>
2021-06-13 08:32:18 -0700

    implement plus_minus

--- 9_first_last.py
+++ 9_first_last.py
@@ -12,8 +12,20 @@ def sum_even_odd_idxs(seq):
     odds = seq[1::2]
     return [sum(odds), sum(evens)]
 
+def plus_minus(seq):
+    plus = True
+    res = seq[0]
+    for idx, el in enumerate(seq[1:]):
+        if plus:
+            res += el
+            plus = False
+        else:
+            res -= el
+            plus = True
+    return res
 #print(firstlast('abc'))
 #print(firstlast([1,2,3]))
 #print(firstlast((1,2,3)))
 
-print(sum_even_odd_idxs([1,2,3,4,5,6]))
+#print(sum_even_odd_idxs([1,2,3,4,5,6]))
+print(plus_minus([10,20,30,40,50,60]))
