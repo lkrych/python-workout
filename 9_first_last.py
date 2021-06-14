2aa463b4db4e63d3658c6013b98daeeef7aad02b
Leland Krych <leland.krych@gmail.com>
2021-06-14 07:40:48 -0700

    first try at myzip

--- 9_first_last.py
+++ 9_first_last.py
@@ -23,9 +23,31 @@ def plus_minus(seq):
             res -= el
             plus = True
     return res
+
+def myzip(*args):
+    # until each iterable has been drained, grab an element of that index and put it into zipped
+    zipped = []
+    idx = 0
+    while True:
+        zipped_item = []
+        still_zipping = False
+        for arg in args:
+            if len(arg) - 1 > idx:
+                zipped_item.append(arg[idx])
+                still_zipping = True
+        zipped.append((*zipped_item))
+        idx += 1
+        if not still_zipping:
+            break
+    return zipped
+
+
+
 #print(firstlast('abc'))
 #print(firstlast([1,2,3]))
 #print(firstlast((1,2,3)))
 
 #print(sum_even_odd_idxs([1,2,3,4,5,6]))
-print(plus_minus([10,20,30,40,50,60]))
+#print(plus_minus([10,20,30,40,50,60]))
+
+print(myzip([1,2,3],['a','b','c']))
