c2a1ec340cd65592be2bf8a3a68bf7a1361f21d1
Leland Krych <leland.krych@gmail.com>
2021-06-18 10:13:41 -0700

    better structured code

--- 14_restaurant.py
+++ 14_restaurant.py
@@ -4,25 +4,20 @@ MENU = {
     'chips': 2,
 }
 
-def get_order():
-    order = input("order: ").strip()
-    total = MENU.get(order, 0)
-    if order == "":
-        return -1
-    if total == 0:
-        print(f"Sorry, we are fresh out of {order} today")
-    else:
-        print(f"{order} costs {total}")
-    return total
-
 def restaurant():
     total = 0
     while True:
-        order_val = get_order()
-        if order_val == -1:
+        order = input('Order: ').strip()
+
+        if not order:
             break
-        total += order_val
-        print(f"New total: {total}")
+        
+        if order in MENU:
+            price = MENU[order]
+            total += price
+            print(f'{order} is {price}, total is {total}')
+        else:
+            print(f'We are fresh out of {order} today')
     print(f"Your total is {total}")
 
 restaurant()
