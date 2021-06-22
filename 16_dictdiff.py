121b26e59f79eb3ee4156d9380cc7b0ce1ef34bc
Leland Krych <leland.krych@gmail.com>
2021-06-22 07:49:12 -0700

    much prettier implementation using get and a set

--- 16_dictdiff.py
+++ 16_dictdiff.py
@@ -1,13 +1,11 @@
 def dictdiff(d1, d2):
     diff = {}
-    for k,v in d1.items():
-        if k not in d2:
-            diff[k] = [v, None]
-        elif v != d2[k]:
-            diff[k] = [v, d2[k]]
-    for k,v in d2.items():
-        if k not in d1:
-            diff[k] = [v, None]
+    all_keys = d1.keys() | d2.keys() #use the union operator to collect all keys
+
+    for key in all_keys:
+        if d1.get(key) != d2.get(key):
+            diff[key] = [d1.get(key), d2.get(key)]
+    
     return diff
 x = {'a': 1, 'b': 2, 'c': 3}
 y = {'a': 2, 'b': 2, 'd': 4}
