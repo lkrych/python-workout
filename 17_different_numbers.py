227fd441f6feea56e099873c69a4f4894b3613da
Leland Krych <leland.krych@gmail.com>
2021-06-23 07:29:32 -0700

    just pile the iterable into the set, don't worry about iterating

--- 17_different_numbers.py
+++ 17_different_numbers.py
@@ -1,7 +1,5 @@
 def how_many_different_numbers(list):
-    s = set()
-    for el in list:
-        s.add(el)
+    s = set(list)
     return len(s)
 
 print(how_many_different_numbers([1,2,3,4,2,3,4]))
