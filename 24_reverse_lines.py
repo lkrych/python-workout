a03a15b378edd1364aed7914046ea61267ffee11
Leland Krych <leland.krych@gmail.com>
2021-08-02 12:59:25 -0700

    nifty condensed version of previous algorithm

--- 24_reverse_lines.py
+++ 24_reverse_lines.py
@@ -1,12 +1,8 @@
 
 
 def reverse_lines(in_file, out_file):
-    fin = open(in_file, "r")
-    fout = open(out_file, "w")
-    lines = fin.readlines()
-    fin.close()
-    for line in lines:
-        fout.write(f'{line.rstrip()[::-1]}\n')
-    fout.close()
+    with open(in_file) as fin, open(out_file, 'w') as fout:
+        for line in fin:
+            fout.write(f'{line.rstrip()[::-1]}\n')
 
 reverse_lines("./testdir/test1.txt", "./testdir/test1-rev.txt")
