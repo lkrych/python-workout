d72e579f24070ecac5ba949029115364d9e5970f
Leland Krych <leland.krych@gmail.com>
2021-08-05 10:48:44 -0700

    edit: use named args instead of *args

--- 25_xml_gen.py
+++ 25_xml_gen.py
@@ -2,7 +2,7 @@
 # myxml('foo', 'bar') -> <foo>bar</foo>
 # myxml('foo', 'bar', a=1, b=2, c=3) -> <foo a="1" b="2" c="3">bar</foo>
 # output will always be a string
-def myxml(*args, **kwargs):
+def myxml(tag, content='', **kwargs):
     xml = ""
     attr = ""
 
@@ -12,10 +12,7 @@ def myxml(*args, **kwargs):
             if idx == 0:
                 attr += " "
             attr += f'{kwarg[0]}="{kwarg[1]}"'
-    if len(args) == 1:
-        xml = f'<{args[0]}{attr}></{args[0]}>'
-    elif len(args) == 2:
-        xml = f'<{args[0]}{attr}>{args[1]}</{args[0]}>'
+    xml = f'<{tag}{attr}>{content}</{tag}>'
 
     return xml
 
