576773f83912284d0e19095eea09eb906071fb07
Leland Krych <leland.krych@gmail.com>
2021-08-05 10:52:07 -0700

    use list comprehension to make function much shorter

--- 25_xml_gen.py
+++ 25_xml_gen.py
@@ -4,14 +4,7 @@
 # output will always be a string
 def myxml(tag, content='', **kwargs):
     xml = ""
-    attr = ""
-
-    if len(kwargs) > 0:
-        for idx, kwarg in enumerate(kwargs.items()):
-            #add space for first attr
-            if idx == 0:
-                attr += " "
-            attr += f'{kwarg[0]}="{kwarg[1]}"'
+    attr = "".join([f' {k}="{v}"' for k,v in kwargs.items()])
     xml = f'<{tag}{attr}>{content}</{tag}>'
 
     return xml
