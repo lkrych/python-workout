cc9418e8883097cc70c0c42c46861136ec2a1275
Leland Krych <leland.krych@gmail.com>
2021-08-10 07:24:27 -0700

    use operator module

--- 26_prefix_notation_calc.py
+++ 26_prefix_notation_calc.py
@@ -1,32 +1,15 @@
 # calculates in prefix notation
 # ex + 3 4 = 3 + 4 = 7
-
-def add(a,b):
-    return a + b
-
-def sub(a,b):
-    return a - b
-
-def mul(a,b):
-    return a * b
-
-def div(a,b):
-    return a / b
-
-def mod(a,b):
-    return a % b
-
-def exp(a,b):
-    return a ** b
+import operator
 
 def calc(to_solve):
     operations = {
-            '+': add,
-            '-': sub,
-            '*': mul,
-            '/': div,
-            '%': mod,
-            '**': exp}
+            '+': operator.add,
+            '-': operator.sub,
+            '*': operator.mul,
+            '/': operator.truediv,
+            '%': operator.mod,
+            '**': operator.pow}
     op, a, b =  to_solve.split()
     a = int(a)
     b = int(b)
